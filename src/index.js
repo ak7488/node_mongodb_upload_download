d56aa4a697e77bf2d6e4769ab6884078cf6a1d12
anubhav anand <anubhav7488599737185@gmail.com>
2021-11-26 15:44:38 +0530

    fixed cors

--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@ const fileRouter = require("./routes/file.js");
 
 const app = express();
 const port = process.env.PORT;
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log(process.env.ALLOWED_ORIGIN.split(","), origin);
+    if (process.env.ALLOWED_ORIGIN.split(",").indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (process.env.ALLOWED_ORIGIN.split(",").indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 app.use(userRouter);
 app.use(fileRouter);
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`);
+  console.log(`Server is up and running on port ${port}`);
 });
